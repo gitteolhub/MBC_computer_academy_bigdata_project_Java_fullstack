c01888c41c26c95eca2b503ea2094ed63a4d2c81
gitteolhub <ht2001@naver.com>
2024-09-11 17:37:14 +0900

    response 속성 text에서 content로 변경 API 응답이 UTF-8이라 content로 바이트 문자열 반환해도 사람이 읽을수 있게 나옴

--- crawling/crawling_test.py
+++ crawling/crawling_test.py
@@ -5,7 +5,7 @@ url = 'http://api.data.go.kr/openapi/tn_pubr_public_nutri_food_info_api'
 params = {'serviceKey' : '보안 인증키 이므로 사용할 때만 입력', 'pageNo' : '1', 'numOfRows' : '100', 'type' : 'json', 'foodCd' : '', 'foodNm' : '', 'dataCd' : '', 'typeNm' : '', 'foodOriginCd' : '', 'foodOriginNm' : '', 'foodLv3Cd' : '', 'foodLv3Nm' : '', 'foodLv4Cd' : '', 'foodLv4Nm' : '', 'foodLv5Cd' : '', 'foodLv5Nm' : '', 'foodLv6Cd' : '', 'foodLv6Nm' : '', 'foodLv7Cd' : '', 'foodLv7Nm' : '', 'nutConSrtrQua' : '', 'enerc' : '', 'water' : '', 'prot' : '', 'fatce' : '', 'ash' : '', 'chocdf' : '', 'sugar' : '', 'fibtg' : '', 'ca' : '', 'fe' : '', 'p' : '', 'k' : '', 'nat' : '', 'vitaRae' : '', 'retol' : '', 'cartb' : '', 'thia' : '', 'ribf' : '', 'nia' : '', 'vitc' : '', 'vitd' : '', 'chole' : '', 'fasat' : '', 'fatrn' : '', 'srcCd' : '', 'srcNm' : '', 'foodSize' : '', 'restNm' : '', 'dataProdCd' : '', 'dataProdNm' : '', 'crtYmd' : '', 'crtrYmd' : '', 'instt_code' : '', 'instt_nm' : '' }
 
 response = requests.get(url, params=params)
-print(response.text)
+print(response.content)
 
 # JSON 파일로 저장
 if response.status_code == 200:  # 성공적인 응답인 경우
