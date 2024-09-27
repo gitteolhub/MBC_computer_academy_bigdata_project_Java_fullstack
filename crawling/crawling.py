1cb5ef1cf4726dfd5007691d62d62451f6db5072
gitteolhub <ht2001@naver.com>
2024-09-27 17:12:15 +0900

    api에서 데이터 확인 후 갱신 및 로그 저장 기능 추가

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import os
+from datetime import datetime
 
 # 공공데이터포털에서 제공하는 식품영양성분 API URL
 url = 'http://api.data.go.kr/openapi/tn_pubr_public_nutri_food_info_api'
@@ -13,11 +15,15 @@ params = {
     # 이 외 명시하지 않은 파라미터에선 모든 데이터를 가져온다.
 }
 
+# 데이터를 저장할 파일 경로
+file_path = 'D:/student/LHT/works/crawling/food_data.json'
+log_file_path = 'D:/student/LHT/works/crawling/update_log.txt'
+
 # 모든 페이지의 데이터를 저장할 리스트 초기화
 all_items = []
 
 # 첫 번째 페이지 요청을 보내 데이터를 가져옴. 데이터에는 전체 데이터 개수인 totalCount가 포함되어 있음
-response = requests.get(url, params=params)
+response = requests.get(url, params = params)
 
 # 응답 상태 코드가 200이면 성공
 if response.status_code == 200:
@@ -58,8 +64,34 @@ else:
     print(f"API 요청 실패: 상태 코드 {response.status_code}")
     print(f"에러 메시지: {response.text}")
 
-# 모든 페이지의 데이터를 JSON 파일로 저장
-with open('D:/student/LHT/works/crawling/food_data.json', 'w', encoding='utf-8') as f:  # 실행하는 PC에 따라 저장경로 지정
-    json.dump(all_items, f, ensure_ascii=False, indent=4)
+# 새로운 데이터를 JSON 형식으로 변환
+new_data_json = json.dumps(all_items, ensure_ascii = False, indent = 4)
 
-print("모든 페이지의 데이터가 food_data.json 파일로 저장되었습니다.")
+# 기존 파일이 있는지 확인하고, 있으면 이전 데이터를 읽어옴
+if os.path.exists(file_path):
+    with open(file_path, 'r', encoding = 'utf-8') as f:
+        old_data_json = f.read()
+
+    # 이전 데이터와 새로운 데이터를 비교
+    if new_data_json == old_data_json:
+        print("데이터가 갱신되지 않았습니다.")
+        update_status = "No Update"
+    else:
+        print("데이터가 갱신되었습니다.")
+        update_status = "Updated"
+        # 새로운 데이터를 파일로 저장
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(new_data_json)
+else:
+    # 파일이 없으면 새로운 데이터를 바로 저장
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(new_data_json)
+    update_status = "First Time Update"
+    print("처음으로 데이터를 저장했습니다.")
+
+# 갱신 여부를 기록하는 로그 파일에 작성
+if update_status != "No Update":
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    with open(log_file_path, 'a', encoding='utf-8') as log_file:
+        log_file.write(f"{current_time} - {update_status} - {response_json['response']['header']['resultMsg']} - {total_count} items\n")
+    print("저장 혹은 갱신된 날짜와 시간이 로그 파일에 기록되었습니다.")
