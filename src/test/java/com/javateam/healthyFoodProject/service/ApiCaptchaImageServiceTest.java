a27c1bba02597696a27767b10b9898f611aa6c64
swgic <jinsowon82@gmail.com>
2024-10-08 19:41:00 +0900

    캡차 api키 테스트 후 삭제

--- src/test/java/com/javateam/healthyFoodProject/service/ApiCaptchaImageServiceTest.java
+++ src/test/java/com/javateam/healthyFoodProject/service/ApiCaptchaImageServiceTest.java
@@ -20,44 +20,44 @@ public class ApiCaptchaImageServiceTest {
 
 	@Autowired
 	public ApiCaptchaNkeyService apiCaptchaNkeyService;
-	
+
 	@Autowired
 	public ApiCaptchaImageService apiCaptchaImageService;
-	
+
 	@Test
 	void test() {
-		
+
 		String clientId = "YOUR_CLIENT_ID";
 		String clientSecret = "YOUR_CLIENT_SECRET";
-		
+
 		Map<String, String> requestHeaders = new HashMap<>();
 		requestHeaders.put("X-Naver-Client-Id", clientId);
 		requestHeaders.put("X-Naver-Client-Secret", clientSecret);
-		
+
 		// 키 발급시 0, 캡차 이미지 비교시 1로 세팅
 		String code    = "0";
 		String apiURL  = "https://openapi.naver.com/v1/captcha/nkey?code=" + code;
 		String keyJson = apiCaptchaNkeyService.get(apiURL, requestHeaders);
-		
+
 		log.info("[keyJson]: {}", keyJson);
-		
+
 		ObjectMapper objectMapper = new ObjectMapper();
 		String key = "";
-		
+
 		try{
 			key = objectMapper.readValue(keyJson, Map.class).get("key").toString();
 			log.info("[key(result)]: {}", key);
-			
+
 		} catch (IOException ex) {
-			
+
 			log.error("[ApiCaptchaImageServiceTest][JSON parsing error]");
 			ex.printStackTrace();
 		}
-		
+
 		apiURL = "https://openapi.naver.com/v1/captcha/ncaptcha.bin?key=" + key;
-		
+
 		String responseBody = apiCaptchaImageService.get(apiURL, requestHeaders);
-		
+
 		log.info("[responseBody]: {}", responseBody);
 	}
 
--- src/test/java/com/javateam/healthyFoodProject/service/ApiCaptchaNkeyServiceTest.java
+++ src/test/java/com/javateam/healthyFoodProject/service/ApiCaptchaNkeyServiceTest.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootTest
 @Slf4j
 public class ApiCaptchaNkeyServiceTest {
-	
+
 	@Autowired
 	public ApiCaptchaNkeyService apiCaptchaNkeyService;
 
@@ -21,18 +21,18 @@ public class ApiCaptchaNkeyServiceTest {
 
 		String clientId     = "YOUR_CLIENT_ID";
 		String clientSecret = "YOUR_CLIENT_SECRET";
-		
+
 		// 키 발급시 0, 캡차 이미지 비교시 1로 세팅
 		String code = "0";
 		String apiURL = "https://openapi.naver.com/v1/captcha/nkey?code=" + code;
-		
+
 		Map<String, String> requestHeaders = new HashMap<>();
 		requestHeaders.put("X-Naver-Client-Id", clientId);
 		requestHeaders.put("X-Naver-Client-Secret", clientSecret);
-		
-		
+
+
 		String responseBody = apiCaptchaNkeyService.get(apiURL, requestHeaders);
-		
+
 		log.info("responseBody: {}", responseBody);
 	}
 
