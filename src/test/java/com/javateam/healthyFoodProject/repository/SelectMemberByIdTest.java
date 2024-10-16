6dd453d48f78b3953953b97ae48c532e103857b7
gitteolhub <ht2001@naver.com>
2024-10-16 11:07:35 +0900

    주석에 기능 설명 명확하게 수정

--- src/test/java/com/javateam/healthyFoodProject/repository/SelectMemberByIdTest.java
+++ src/test/java/com/javateam/healthyFoodProject/repository/SelectMemberByIdTest.java
@@ -18,7 +18,7 @@ class SelectMemberByIdTest {
 	@Autowired
 	public MemberDAO memberDAO;
 	
-	// 아이디와 회원 번호가 같은지 테스트
+	// 아이디를 통한 회원번호 조회 테스트
 	@Test
 	void testSelectMemberByIdNum() {
 		String strTestId="abcd2222";
@@ -27,32 +27,32 @@ class SelectMemberByIdTest {
 		assertEquals("21", objMemberVO.getNum());  // ""안에 조회할 회원번호 입력
 	}
 
-	// 아이디와 회원명이 같은지 테스트
+	// 아이디를 통한 회원명 조회 테스트
 	@Test
 	void testSelectMemberByIdName() {
-		String strTestId="abcd1111";
+		String strTestId="abcd2222";
 		
 		MemberVO objMemberVO = memberDAO.selectMemberById(strTestId);
 		log.info("[objMemberVO]: " + objMemberVO);
 		
-		assertEquals("홍길동", objMemberVO.getName());
+		assertEquals("남우민", objMemberVO.getName());
 	}
 
-	// 아이디와 비밀번호가 같은지 테스트
+	// 아이디를 통한 비밀번호 조회 테스트
 	@Test
 	void testSelectMemberByIdPw() {
-		String strTestId="abcd1111";
+		String strTestId="abcd2222";
 		
 		MemberVO objMemberVO = memberDAO.selectMemberById(strTestId);
-		assertEquals("#Abcd1234", objMemberVO.getPw());
+		assertEquals("$2a$10$r1KdA1ZOiRCmHBtf3qk3DuaoxKCnch7kzdlw6cRqV8u8XNBts.uDq", objMemberVO.getPw());
 	}
 	
-	// 아이디와 이메일이 같은지 테스트
+	// 아이디를 통한 이메일 조회 테스트
 	@Test
 	void testSelectMemberByIdEmail() {
-		String strTestId="abcd1111";
+		String strTestId="abcd2222";
 		
 		MemberVO objMemberVO = memberDAO.selectMemberById(strTestId);
-		assertEquals("abcd1111@abcd.com", objMemberVO.getEmail());
+		assertEquals("abcd3333@abcd.com", objMemberVO.getEmail());
 	}
 }
