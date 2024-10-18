921c4af98ef4efd7449cf4a74149f85906513d90
gitteolhub <ht2001@naver.com>
2024-10-18 12:26:00 +0900

    주석 명확하게 변경

--- src/test/java/com/javateam/healthyFoodProject/repository/HasMemberForUpdateTest.java
+++ src/test/java/com/javateam/healthyFoodProject/repository/HasMemberForUpdateTest.java
@@ -16,52 +16,48 @@ class HasMemberForUpdateTest {
 	public MemberDAO memberDAO;
 	
 	// 이메일 중복 점검
-	// 회원 기준 메일로 재사용 점검
+	// 자신의 기존 메일 입력시 성공하도록 테스트
 	@Test
 	void testHasMemberForMyEmailUpdate() {
 		
-		assertFalse(memberDAO.hasMemberForUpdate("abcd1111", "EMAIL", "abcd1111@abcd.com"));
+		assertFalse(memberDAO.hasMemberForUpdate("abcd2222", "EMAIL", "abcd3333@abcd.com"));
 	}
 	
-	// 신규 메일 사용
-	// 다른 회원 이메일과 중복되는 메일 사용 점검
+	// 다른 회원의 이메일과 중복되는 메일 사용시 실패하도록 테스트
 	@Test
 	void testHasMemberForDuplicateEmail() {
 		
-		assertTrue(memberDAO.hasMemberForUpdate("abcd1111", "EMAIL", "abcd2222@abcd.com"));
+		assertTrue(memberDAO.hasMemberForUpdate("abcd2222", "EMAIL", "abcd1234@Abcd.com"));
 		
 	}
 	
-	// 신규 메일 사용
-	// 다른 회원 이메일과 중복되지 않는 메일 사용 점검
+	// 다른 회원의 이메일과 중복되지 않는 메일 사용시 성공하도록 테스트
 	@Test
 	void testHasMemberForEmail() {
 		
-		assertFalse(memberDAO.hasMemberForUpdate("abcd1111", "EMAIL", "ABCD8787@abcd.com"));
+		assertFalse(memberDAO.hasMemberForUpdate("abcd2222", "EMAIL", "ABCD8787@abcd.com"));
 	}
 
 	// 연락처 중복 점검
-	// 회원 기준 연락처로 재사용 점검
+	// 자신의 기존 연락처 입력시 성공하도록 테스트
 	@Test
 	void testHasMemberForMyPhoneUpdate() {
 		
-		assertFalse(memberDAO.hasMemberForUpdate("abcd1111", "PHONE", "010-1111-3333"));
+		assertFalse(memberDAO.hasMemberForUpdate("abcd2222", "PHONE", "010-2222-1234"));
 	}
 	
-	// 신규 연락처 사용
-	// 다른 회원 연락처와 중복되는 연락처 사용 점검
+	// 다른 회원의 연락처와 중복되는 연락처 사용시 실패하도록 테스트
 	@Test
 	void testHasMemberForDuplicatePhone() {
 		
-		assertTrue(memberDAO.hasMemberForUpdate("abcd1111", "PHONE", "010-2222-1234"));
+		assertTrue(memberDAO.hasMemberForUpdate("abcd2222", "PHONE", "010-5678-1034"));
 		
 	}
 	
-	// 신규 연락처 사용
-	// 다른 회원 연락처와 중복되지 않는 연락처 사용 점검
+	// 다른 회원의 연락처와 중복되지 않는 연락처 사용시 성공하도록 테스트
 	@Test
 	void testHasMemberForPhone() {
 		
-		assertFalse(memberDAO.hasMemberForUpdate("abcd1111", "PHONE", "010-1234-5678"));
+		assertFalse(memberDAO.hasMemberForUpdate("abcd2222", "PHONE", "010-1234-5678"));
 	}
 }
