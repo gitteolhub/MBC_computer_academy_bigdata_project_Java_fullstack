99a49e7cdc1ea91eac01391d31b1e6b0a621b282
swgic <jinsowon82@gmail.com>
2024-10-21 17:17:20 +0900

    controller loginFormTest 이름 변경, 에러 수정

--- src/test/java/com/javateam/healthyFoodProject/controller/LoginFormTest.java
+++ src/test/java/com/javateam/healthyFoodProject/controller/LoginFormTest.java
@@ -28,64 +28,64 @@ import lombok.extern.slf4j.Slf4j;
 @AutoConfigureMockMvc
 @WebAppConfiguration
 @Slf4j
-public class LoginTest {
+public class LoginFormTest {
 
 	@Autowired
 	private WebApplicationContext webApplicationcontext;
-	
+
 	@Autowired
 	private FilterChainProxy springSecurityFilterChain;
-	
+
 	@Autowired
 	private AuthController authController;
-	
+
 	private MockMvc mockMvc;
-	
+
 	private String strId;
 	private String strPw;
-	
+
 	@BeforeEach
 	public void init() {
 		mockMvc = MockMvcBuilders.standaloneSetup(authController)					// 인증 컨트롤러를 세팅
 								 .apply(springSecurity(springSecurityFilterChain))	// springSecurity 적용
 								 .build();
 	}
-	
+
 	@DisplayName("로그인 테스트: 아이디,비밀번호 일치할 경우(정상 가입된 회원정보)")
 	@Test
 	public void testAuthIdPw() throws Exception {
-		
+
 		strId = "abcd1111";
 		strPw = "#Abcd1234";
-		
-		mockMvc.perform(formLogin("/login")						 // 로그인 URL로 post 요청
+
+		mockMvc.perform(formLogin("/loginForm")						 // 로그인 URL로 post 요청
 					.user("userid", strId)						 // 회원 아이디 설정
 					.password("password", strPw))				 // 회원 비밀번호 설정
 			   .andExpect(authenticated().withUsername(strId))	 // 인증 성공 시 아이디 확인
 			   .andDo(print());									 // 요청과 응답을 출력
 	}
-	
+
 	@DisplayName("로그인 테스트: 아이디가 일치하지만, 비밀번호가 불일치할 경우(정상 가입된 회원정보)")
 	@Test
 	public void testAuthId() throws Exception {
-		
+
 		strId = "abcd1234";
 		strPw = "#Abcd12345";
-		
-		MvcResult mvcResult = mockMvc.perform(formLogin("/login")
+
+		MvcResult mvcResult = mockMvc.perform(formLogin("/loginForm")
 										.user("userid", strId)
 										.password("password", strPw))
 									 .andExpect(unauthenticated())				// 인증 실패 확인
 									 .andExpect(redirectedUrl("/loginError"))	// 로그인 실패 시 redirected URL 확인
 									 .andDo(print())
 									 .andReturn();								// 결과 반환
-		
+
 		// 세션에서 예외 메시지를 가져옴
 		String sessionMsg = mvcResult.getRequest()
 									 .getSession()
 									 .getAttribute("SPRING_SECURITY_LAST_EXCEPTION")
 									 .toString();
-		
+
 		// 기대하는 메시지를 세션 메시지에 표함되어 있는지 확인
 		assertThat("비밀번호가 일치하지 않습니다.").isSubstringOf(sessionMsg);
 	}
@@ -93,23 +93,23 @@ public class LoginTest {
 	@DisplayName("로그인 테스트 : 아이디도 없고 비밀번호 불일치할 경우(DB에 등록되지 않은 회원정보)")
 	@Test
 	public void testAuth() throws Exception {
-		
+
 		strId = "abcd12345";
 		strPw = "Abcd1111!";
-		
-		MvcResult mvcResult = mockMvc.perform(formLogin("/login")
+
+		MvcResult mvcResult = mockMvc.perform(formLogin("/loginForm")
 										.user("userid", strId)
 										.password("password", strPw))
 									 .andExpect(unauthenticated())				// 인증 실패 확인
 									 .andExpect(redirectedUrl("/loginError"))	// 로그인 실패 시 redirected URL 확인
 									 .andDo(print())
 									 .andReturn();								// 결과 반환
-		
+
 		String sessionMsg = mvcResult.getRequest()
 									 .getSession()
 									 .getAttribute("SPRING_SECURITY_LAST_EXCEPTION")
 									 .toString();
-		
+
 		assertThat("회원 아이디가 없습니다.").isSubstringOf(sessionMsg);
 	}
 
