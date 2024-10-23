e3216ffaacde7002d60d8db11d96611693b02a88
swgic <jinsowon82@gmail.com>
2024-10-23 09:02:17 +0900

    log.error 더 찾기 쉽게 수정

--- src/main/java/com/javateam/healthyFoodProject/service/DeleteCaptchaImageService.java
+++ src/main/java/com/javateam/healthyFoodProject/service/DeleteCaptchaImageService.java
@@ -14,27 +14,27 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class DeleteCaptchaImageService {
-	
+
 	// 캡차 이미지 파일을 삭제하는 메서드
 	public void deleteCaptchaImage() {
-		
+
 		Path path = null;
 		ClassPathResource classPathResource = new ClassPathResource("/static/captcha/image/");
-		
+
 		try {
 			path = Paths.get(classPathResource.getURI());
-			
+
 		} catch (IOException ex) {
-			log.error("경로 error");
+			log.error("[DeleteCaptchaImageService] 경로 에러");
 			ex.printStackTrace();
 		}
-		
+
 		try {
 			// 지정된 경로의 모든 파일 삭제
 			FileUtils.cleanDirectory(new File(path.toString()));
-			
+
 		} catch (IOException ex) {
-			log.error("captcha 이미지 파일 삭제 오류");
+			log.error("[DeleteCaptchaImageService] captcha 이미지 파일 삭제 오류");
 			ex.printStackTrace();
 		}
 	}
