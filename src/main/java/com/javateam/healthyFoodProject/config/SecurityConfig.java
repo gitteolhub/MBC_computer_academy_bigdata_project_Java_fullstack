9baeae53ee7c2e7d3cb9a624c12f59975dc9c61b
swgic <jinsowon82@gmail.com>
2024-10-23 10:15:42 +0900

    security에 "/foodMenu" 추가

--- src/main/java/com/javateam/healthyFoodProject/config/SecurityConfig.java
+++ src/main/java/com/javateam/healthyFoodProject/config/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
 											   authorizeHttpRequests.requestMatchers("/",                    "/demo",                "/resources/**",  "/loginError",      "/join",
 													   							 	 "/joinAjax",            "/joinDemo",            "/joinAjaxDemo",  "/member/joinProc", "/member/joinProc2",
 													   							 	 "/member/joinProcDemo", "/member/joinProcAjax", "/login/idCheck", "/loginForm",       "/member/hasFld/**",
-													   							 	 "/home")
+													   							 	 "/home", "/foodMenu")
 											   						.permitAll()
 											   						.requestMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")				// ROLE_ADMIN 권한이 필요한 경로
 											   						.requestMatchers("/secured/**", "/myPage", "/member/view", "/member/hasFldForUpdate/**", "/member/update",
