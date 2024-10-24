9b5657fd34cf4cf0f20d6ec012c09c60c7648930
swgic <jinsowon82@gmail.com>
2024-10-24 15:12:13 +0900

    foodMenu 테스트 확인 후 원래상태로

--- src/main/java/com/javateam/healthyFoodProject/controller/AuthController.java
+++ src/main/java/com/javateam/healthyFoodProject/controller/AuthController.java
@@ -53,7 +53,7 @@ public class AuthController {
 	public String foodMenu() {
 
 		log.info("[foodMenu]");
-		return "/food/view";
+		return "foodMenu";
 	}
 
 	@GetMapping({"/login/oauth2/authorization/naver",
