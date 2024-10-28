fdf8b4309b47561157cb5fadf91ea5a1bad222db
swgic <jinsowon82@gmail.com>
2024-10-28 12:25:09 +0900

    초기식단이 화면이 보이도록 전체적으로 수정

--- src/main/java/com/javateam/healthyFoodProject/config/SecurityConfig.java
+++ src/main/java/com/javateam/healthyFoodProject/config/SecurityConfig.java
@@ -75,7 +75,8 @@ public class SecurityConfig {
 											   						.permitAll()
 											   						.requestMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")				// ROLE_ADMIN 권한이 필요한 경로
 											   						.requestMatchers("/secured/**",        "/myPage",   "/member/view",   "/member/hasFldForUpdate/**", "/member/update",
-											   										 "/member/updateProc", "/foodMenu", "/foodMenu/like", "/foodMenu/dislike",          "/foodMenu/refresh")
+											   										 "/member/updateProc", "/foodMenu", "/foodMenu/like", "/foodMenu/dislike",          "/foodMenu/refresh",
+											   										 "/foodMenu/view",     "/foodMenu/viewJson")
 											   						.hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")	// ROLE_USER, ROLE_ADMIN 권한이 필요한 경로
 											   						.anyRequest().authenticated());
 
