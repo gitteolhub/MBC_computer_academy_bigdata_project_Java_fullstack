4ec2bbff44e1167f3bf51c8e23ff1f5e46c54ea3
swgic <jinsowon82@gmail.com>
2024-10-31 12:12:13 +0900

    tonicjh 프로젝트에 저장된 새로운 사용자별 식단 데이터파일 연결 (테스트 전)

--- src/main/java/com/javateam/healthyFoodProject/service/JsonService.java
+++ src/main/java/com/javateam/healthyFoodProject/service/JsonService.java
@@ -1,11 +1,14 @@
 package com.javateam.healthyFoodProject.service;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +31,8 @@ public class JsonService {
 
 	@Autowired
 	public ChosenFoodMenuDAO chosenFoodMenuDAO;
+
+	public ArrayList<String> updatingStrId = new ArrayList<String>();
 
 	public void saveMemberDataJson() {
 		List<MemberJsonVO> allUserData = memberService.selectAllMembersJson();
@@ -71,20 +76,48 @@ public class JsonService {
 	}
 
 	// 선택된 식단 전체 json 파일로 저장
-	public void saveChosenFoodMenuJson() {
+	public void saveChosenFoodMenuJson(String strId) {
+		updatingStrId.add(strId);
+		String strFoodMenu = "";
+		String strFoodMenuResult = "";
+
 		List<ChosenFoodMenuVO> chosenFoodMenus = chosenFoodMenuDAO.selectAllFoodMenu();
 
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		for (int i=0; i < chosenFoodMenus.size(); i++) {
+			if(!updatingStrId.contains(chosenFoodMenus.get(i).getId())) {
+				strFoodMenu = chosenFoodMenus.get(i).getFoodmenu();
+				strFoodMenuResult = chosenFoodMenus.get(i).getFoodmenuResult();
+			}
+		}
+
 		String chosenFoodMenuFilePath = "src/main/resources/JsonDataFiles/chosenFoodMenu_Json.json";
 
-		try {
-			String json = gson.toJson(chosenFoodMenus);
-			Files.write(Paths.get(chosenFoodMenuFilePath), json.getBytes());
-			log.info("[saveChosenFoodMenuJson]: {}", chosenFoodMenuFilePath);
+		 File file = new File(chosenFoodMenuFilePath);
+		 try {
+			 FileWriter fileWriter = new FileWriter(file, true);
+			 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-		} catch (IOException ex){
-			log.error("[saveChosenFoodMenuJson] IOException: {}", ex);
-		}
+			 String strWriting = "";
+
+			 if(file.createNewFile()) {
+				 log.info("[chosenFoodMenu_Json 파일이 없어 새로 만들었습니다.]");
+
+				 strWriting = "{";
+
+			 } else {
+				 log.info("[chosenFoodMenu_Json 파일이 있습니다.]");
+				 strWriting = ",{";
+			 }
+			 strWriting += strId + ":";
+			 strWriting += "[" + strFoodMenu + "]|";
+			 strWriting += strFoodMenuResult + "}";
+
+			 bufferedWriter.write(strWriting);
+			 bufferedWriter.close();
+
+		 } catch(IOException ex) {
+			log.error("[saveChosenFoodMenuJson][IOException]: {}", ex);
+		 }
 
 	}
 
