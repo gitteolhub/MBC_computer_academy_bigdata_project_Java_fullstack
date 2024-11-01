79eee7586bbf99cc563bdfb0eb5979a1d59eb686
swgic <jinsowon82@gmail.com>
2024-11-01 15:17:39 +0900

    코드 정리

--- src/test/java/com/javateam/healthyFoodProject/repository/ChosenFoodMenuDAOTest.java
+++ src/test/java/com/javateam/healthyFoodProject/repository/ChosenFoodMenuDAOTest.java
@@ -22,16 +22,20 @@ class ChosenFoodMenuDAOTest {
 
 	@Test
 	void testSelectAllFoodMenu2() {
+
 		ArrayList<String> updatingStrId = new ArrayList<String>();
+
 		updatingStrId.add("abcd6666");
 		String strFoodMenu = "";
 		String strFoodMenuResult = "";
 		List<ChosenFoodMenuVO> chosenFoodMenus = chosenFoodMenuDAO.selectAllFoodMenu();
+
 		for (int i=0; i < chosenFoodMenus.size(); i++) {
 			log.info("[saveChosenFoodMenuJson][chosenFoodMenus.get(i)]: {}", chosenFoodMenus.get(i));
 			log.info("[saveChosenFoodMenuJson][chosenFoodMenus.get(i).getId() type]: {}", chosenFoodMenus.get(i).getId()==null? "null입니다." : chosenFoodMenus.get(i).getId().getClass().getName());
 			log.info("[saveChosenFoodMenuJson][chosenFoodMenus.get(i).getFoodmenu() type]: {}", chosenFoodMenus.get(i).getFoodmenu()==null? "null입니다." : chosenFoodMenus.get(i).getFoodmenu().getClass().getName());
 			log.info("[saveChosenFoodMenuJson][chosenFoodMenus.get(i).getFoodmenuResult() type]: {}", chosenFoodMenus.get(i).getFoodmenuResult()==null? "null입니다." : chosenFoodMenus.get(i).getFoodmenuResult().getClass().getName());
+
 			if(!updatingStrId.contains(chosenFoodMenus.get(i).getId())) {
 				strFoodMenu = chosenFoodMenus.get(i).getFoodmenu();
 				strFoodMenuResult = chosenFoodMenus.get(i).getFoodmenuResult();
