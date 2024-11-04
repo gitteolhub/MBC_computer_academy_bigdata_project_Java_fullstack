0f7dca96429af78d186eee5499b0e2fe117d5e07
YSJANG26 <dudtjs1543@naver.com>
2024-11-04 10:52:32 +0900

    memberupdate.js 모바일 삭제

--- src/main/resources/static/js/member_update.js
+++ src/main/resources/static/js/member_update.js
@@ -73,15 +73,6 @@ window.onload = function(e) {
 	// 이메일 중복 점검 플래그
 	let emailDuplicatedCheckFlag = false;
 
-    // 연락처 점검 플래그(회원 수정시 기존 값이 필수값이므로 true 설정)
-    let mobileCheckFlag = true;
-
-	// 연락처(휴대폰) 중복 점검 플래그
-	let mobileDuplicatedCheckFlag = false;
-	
-	// 연락처(집전화) 점검 플래그(회원 수정시 기존 값이 필수값이므로 true 설정)
-	let phoneCheckFlag = true;
-	
     // 주소 점검 플래그
     // 주소에 대한 사항이 필수가 아니라 선택 사항인 경우는 
     // 입력하지 않아도 무관하기 때문에 초기 상태(무입력 상태)도 true로 간주하여
@@ -103,17 +94,6 @@ window.onload = function(e) {
     // 이메일 필드 에러 패널 인식
     let emailFldErrPnl = document.getElementById("email_fld_err_pnl");
 
-    // 연락처(휴대폰) 필드 인식
-    let mobileFld = document.getElementById("mobile");
-
-    // 연락처(휴대폰) 필드 에러 패널 인식
-    let mobileFldErrPnl = document.getElementById("mobile_fld_err_pnl");
-
-	// 연락처(집전화) 필드 인식
-    let phoneFld = document.getElementById("phone");
-
-	// 연락처(집전화) 필드 에러 패널 인식
-    let phoneFldErrPnl = document.getElementById("phone_fld_err_pnl");
 
     ////////////////////////////////////////////////////////////////////////
 
@@ -275,48 +255,7 @@ window.onload = function(e) {
                         "회원 이메일을 제시된 예와 같이 작성해주세요.");
     } //     
 
-    // 연락처(휴대폰) 필드 입력 후 이벤트 처리 : onkeyup
-    mobileFld.onkeyup = (e) => {
 
-        console.log("연락처(휴대폰) 필드 onkeyup")
-        // 연락처 필드 유효성 점검(validation)
-        // 기준)
-        /*
-            1) 휴대폰 입력 예시 : ex) 010-1234-5678
-            2) regex(정규표현식) : /^010-\d{4}-\d{4}$/
-            3) 메시징 : 회원 연락처(휴대폰)를 제시된 예와 같이 작성해주세요.
-        */
-        mobileCheckFlag = isCheckFldValid(mobileFld,
-                        /^010-\d{4}-\d{4}$/,
-                        mobileFld.value,
-                        mobileFldErrPnl,
-                        "회원 연락처(휴대폰)를 제시된 예와 같이 작성해주세요.");
-    } //     
-
-
-    // 연락처(집전화) 필드 입력 후 이벤트 처리 : onkeyup
-    phoneFld.onkeyup = (e) => {
-
-        console.log("연락처(집전화) 필드 onkeyup")
-        // 연락처 필드 유효성 점검(validation)
-        // 기준)
-        /*
-            1) 휴대폰 입력 예시 : ex) 02-111-5678
-            2) regex(정규표현식) : /^0\d{1,2}-\d{3,4}-\d{4}$/
-            3) 메시징 : 회원 연락처(휴대폰)를 제시된 예와 같이 작성해주세요.
-        */
-        phoneCheckFlag = isCheckFldValid(phoneFld,
-                        /^0\d{1,2}-\d{3,4}-\d{4}$/,
-                        phoneFld.value,
-                        phoneFldErrPnl,
-                        "회원 연락처(집전화)를 제시된 예와 같이 작성해주세요.");
-    } //     
-
-
-    ///////////////////////////////////////////////////////////////////////
-
-
-	// 이메일/연락처(휴대폰) 중복 점검
 	
 	// 이메일 중복 점검 : AJAX axios
 	emailFld.onblur = (e) => {
@@ -350,39 +289,6 @@ window.onload = function(e) {
 			 });
 	} //
 	
-	// 연락처(휴대폰) 중복 점검 : AJAX axios
-	mobileFld.onblur = (e) => {
-		
-		console.log("연락처(휴대폰) 중복 점검");
-		
-		// 중복 점검 REST 주소(회원 정보 수정 전용)/인자(id 추가) 변경
-		var idFld = document.getElementById("id"); // 아이디 필드
-	
-		axios.get(`/memberProject/member/hasFldForUpdate/${idFld.value}/MOBILE/${mobileFld.value}`)
-			 .then(function(response) {
-				
-				mobileDuplicatedCheckFlag = response.data;
-				console.log("response.data : ", response.data);
-
-				let mobileDupErrMsg = mobileDuplicatedCheckFlag == true ? "중복되는 연락처(휴대폰)가 존재합니다" : "사용가능한 연락처(휴대폰)입니다"				   
-
-				if (mobileDuplicatedCheckFlag == true) {
-					mobileFldErrPnl.innerHTML = mobileDupErrMsg;
-					// CSS 상하 여백 조정
-					mobileFldErrPnl.style.paddingBottom = '20px';
-				} else {
-					mobileFldErrPnl.innerHTML = ""; // 메시지 초기화
-					mobileFldErrPnl.style.paddingBottom = 0; // 메시지 패널 크기 초기화
-				}	
-					
-			 })
-			 .catch(function(err) {
-				console.error("연락처(휴대폰) 중복 점검 중 서버 에러가 발견되었습니다.");
-				// mobileDuplicatedCheckFlag = false;				
-			 });
-			
-	} //
-
 
     /////////////////////////////////////////////////////////////////
  
@@ -402,14 +308,10 @@ window.onload = function(e) {
 
         // 이메일 및 연락처 점검 플래그
         console.log(`이메일 점검 플래그(emailCheckFlag) : ${emailCheckFlag}`);
-        console.log(`연락처(휴대폰) 점검 플래그(mobileCheckFlag) : ${mobileCheckFlag}`);
-		
-		console.log(`연락처(집전화) 점검 플래그(phoneCheckFlag) : ${phoneCheckFlag}`);
 		
 		// 이메일/연락처(휴대폰) 중복 점검 플래그
 		// 주의) 이 플래그들은 false 이어야 중복되지 않는 값을 의미합니다.  
 		console.log(`이메일 중복 점검 플래그(emailDuplicatedCheckFlag) : ${emailDuplicatedCheckFlag}`);
-		console.log(`연락처(휴대폰) 중복 점검 플래그(mobileDuplicatedCheckFlag) : ${mobileDuplicatedCheckFlag}`);
 		
 		//////////////////////////////////////////////////////////////////////
 		//		
@@ -427,11 +329,9 @@ window.onload = function(e) {
 			pw2CheckFlag == true &&
 			pwEqualCheckFld == true &&
             emailCheckFlag == true &&
-            mobileCheckFlag == true &&
-            phoneCheckFlag == true &&
-			emailDuplicatedCheckFlag == false &&
-			mobileDuplicatedCheckFlag == false)
+			emailDuplicatedCheckFlag == false)
         {
+			
             alert("전송");
 
 			// e.preventDefault();
@@ -455,33 +355,9 @@ window.onload = function(e) {
 
             } //    
 
-            // 연락처 필드 재점검
-            if (mobileCheckFlag == false) {
-
-                mobileCheckFlag = isCheckFldValid(mobileFld,
-                        /^010-\d{4}-\d{4}$/,
-                        mobileFld.value, 
-                        mobileFldErrPnl,
-                        "회원 연락처(휴대폰)를 제시된 예와 같이 작성해주세요.");
-            } // 
-
- 			// 연락처(집전화) 필드 재점검
-            if (phoneCheckFlag == false) {
-
-                phoneCheckFlag = isCheckFldValid(phoneFld,
-                        /^0\d{1,2}-\d{3,4}-\d{4}$/,
-                        phoneFld.value,
-                        phoneFldErrPnl,
-                        "회원 연락처(집전화)를 제시된 예와 같이 작성해주세요.");
-            } // 
-
 			// 이메일/연락처(휴대폰) 중복 재점검에 따른 최종 메시징			
 			if (emailDuplicatedCheckFlag == true) {
 				alert("중복되는 이메일이 존재합니다");
-			}
-			
-			if (mobileDuplicatedCheckFlag == true) {
-				alert("중복되는 연락처(휴대폰)가 존재합니다");
 			}
 			
 			// password1 과 password2의 값의 동일성 여부도 점검  
@@ -495,4 +371,4 @@ window.onload = function(e) {
 
     } // frm.onsubmit ...
 
-} // window.onload ...
+} // window.onload .....
